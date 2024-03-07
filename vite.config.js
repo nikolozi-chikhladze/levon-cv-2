3eeff720ee402ad43111e5be15d83ab706f843d2
Nikolozi Chikhladze <ch1khladze@Nikolozis-MacBook-Pro.local>
2024-03-08 00:01:11 +0700

    chore: add readme

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
-  base: "/levon-cv-2/"
+  // base: "/levon-cv-2/"
 })
